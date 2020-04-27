709007781340d0399d3e38b8aecbe34be5e464e4
Oliver Daff <oliverdaff@users.noreply.github.com>
2020-04-27 15:35:17 +1000

    use static strings

--- src/network_ioc.rs
+++ src/network_ioc.rs
@@ -22,8 +22,8 @@
      ipv6s: Vec<NetworkIOC<'a>>
  }
 
- const URL_PATTERN: &str  = r#"(\b((http|https|hxxp|hxxps|nntp|ntp|rdp|sftp|smtp|ssh|tor|webdav|xmpp)://[\S]{1,})\b)"#; 
- const DOMAIN_PATTERN: &str = r#"([A-Za-z0-9-]+(\.[A-Za-z0-9-]+)*\.(abogado|ac|academy|accountants|active|actor|ad|adult|ae|aero|af|ag|
+ const URL_PATTERN: &'static str  = r#"(\b((http|https|hxxp|hxxps|nntp|ntp|rdp|sftp|smtp|ssh|tor|webdav|xmpp)://[\S]{1,})\b)"#; 
+ const DOMAIN_PATTERN: &'static str = r#"([A-Za-z0-9-]+(\.[A-Za-z0-9-]+)*\.(abogado|ac|academy|accountants|active|actor|ad|adult|ae|aero|af|ag|
      agency|ai|airforce|al|allfinanz|alsace|am|amsterdam|an|android|ao|aq|aquarelle|ar|archi|army|arpa|as|asia|associates|at|
      attorney|au|auction|audio|autos|aw|ax|axa|az|ba|band|bank|bar|barclaycard|barclays|bargains|bayern|bb|bd|be|beer|berlin|
      best|bf|bg|bh|bi|bid|bike|bingo|bio|biz|bj|black|blackfriday|bloomberg|blue|bm|bmw|bn|bnpparibas|bo|boo|boutique|br|
@@ -56,11 +56,11 @@
      vet|vg|vi|viajes|video|villas|vision|vlaanderen|vn|vodka|vote|voting|voto|voyage|vu|wales|wang|watch|webcam|website|wed|wedding|wf|
      whoswho|wien|wiki|williamhill|wme|work|works|world|ws|wtc|wtf|xyz|yachts|yandex|ye|yoga|yokohama|youtube|yt|za|zm|zone|zuerich|zw)\b)"#;
 
- const EMAIL_PATTERN : &str = r#"[A-Za-z0-9_.]+@[0-9a-z.-]+"#;
+ const EMAIL_PATTERN : &'static str = r#"[A-Za-z0-9_.]+@[0-9a-z.-]+"#;
 
- const IPV4_PATTERN : &str = r#"(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)[?\.]?){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"#;
+ const IPV4_PATTERN : & 'static str = r#"(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)[?\.]?){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"#;
 
- const IPV6_PATTERN : &str = r#"(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|
+ const IPV6_PATTERN : &'static str = r#"(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|
                              ([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}
                              (:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:
                              ((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]
@@ -144,6 +144,7 @@
      .collect();
 
  }
+
 
  pub fn parse_emails(input: &str) -> Vec<NetworkIOC> {
      lazy_static! {
