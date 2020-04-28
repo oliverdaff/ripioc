b0fdcf76c16d992a396fc12f5de4ad8c88c216ac
Oliver Daff <oliverdaff@users.noreply.github.com>
2020-04-28 15:40:55 +1000

    formated code

--- src/hash_ioc.rs
+++ src/hash_ioc.rs
@@ -12,7 +12,7 @@ pub enum HashIOC<'a> {
     SHA1(&'a str),
     SHA256(&'a str),
     SHA512(&'a str),
-    SSDEEP(&'a str)
+    SSDEEP(&'a str),
 }
 
 const MD5_PATTERN: &'static str = r#"\b[A-Fa-f0-9]{32}\b"#;
@@ -21,20 +21,22 @@ const SHA1_PATTERN: &'static str = r#"\b[A-Fa-f0-9]{40}\b"#;
 
 pub fn parse_md5(input: &str) -> Vec<HashIOC> {
     lazy_static! {
-        static ref MD5_RE: Box<Regex>= compile_re(Cow::from(MD5_PATTERN));
+        static ref MD5_RE: Box<Regex> = compile_re(Cow::from(MD5_PATTERN));
     }
-    return MD5_RE.find_iter(input)
-    .map(|x|HashIOC::MD5(x.as_str()))
-    .collect()
+    return MD5_RE
+        .find_iter(input)
+        .map(|x| HashIOC::MD5(x.as_str()))
+        .collect();
 }
 
 pub fn parse_sha1(input: &str) -> Vec<HashIOC> {
     lazy_static! {
-        static ref SHA1_RE: Box<Regex>= compile_re(Cow::from(SHA1_PATTERN));
+        static ref SHA1_RE: Box<Regex> = compile_re(Cow::from(SHA1_PATTERN));
     }
-    return SHA1_RE.find_iter(input)
-    .map(|x|HashIOC::SHA1(x.as_str()))
-    .collect()
+    return SHA1_RE
+        .find_iter(input)
+        .map(|x| HashIOC::SHA1(x.as_str()))
+        .collect();
 }
 
 #[cfg(test)]
@@ -43,13 +45,17 @@ mod tests {
 
     #[test]
     fn test_parse_md5() {
-        assert_eq!(parse_md5("this ioc 08f2eb5f1bcbaf25ba97aef26593ed96"), 
-        vec![HashIOC::MD5("08f2eb5f1bcbaf25ba97aef26593ed96")])
+        assert_eq!(
+            parse_md5("this ioc 08f2eb5f1bcbaf25ba97aef26593ed96"),
+            vec![HashIOC::MD5("08f2eb5f1bcbaf25ba97aef26593ed96")]
+        )
     }
 
     #[test]
-    fn test_parse_sha1(){
-        assert_eq!(parse_sha1("this is a ioc a6b2fa823815336bb7352b02a93c970df51f66e8"), 
-    vec![HashIOC::SHA1("a6b2fa823815336bb7352b02a93c970df51f66e8")]);
+    fn test_parse_sha1() {
+        assert_eq!(
+            parse_sha1("this is a ioc a6b2fa823815336bb7352b02a93c970df51f66e8"),
+            vec![HashIOC::SHA1("a6b2fa823815336bb7352b02a93c970df51f66e8")]
+        );
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
-#[macro_use] extern crate lazy_static;
+#[macro_use]
+extern crate lazy_static;
 extern crate regex;
 
-pub mod network_ioc;
 pub mod hash_ioc;
+pub mod network_ioc;
 
 mod regex_builder;
