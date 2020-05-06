9ec3ee92ce164c1b1e6a22ce7f2af544b4b27e86
Oliver Daff <oliverdaff@users.noreply.github.com>
2020-05-06 20:17:02 +1000

    added added cve parser

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ extern crate regex;
 pub mod hash_ioc;
 pub mod network_ioc;
 pub mod file_ioc;
+pub mod cve_ioc;
 
 mod regex_builder;
 
@@ -15,12 +16,16 @@ use crate::hash_ioc::parse_hash_iocs;
 use crate::file_ioc::parse_file_iocs;
 use crate::file_ioc::FileIOCS;
 
+use crate::cve_ioc::parse_cve;
+use crate::cve_ioc::CVEIOC;
+
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct IOCS<'a> {
     network_iocs : NetworkIOCS<'a>,
     hash_iocs : HashIOCS<'a>,
     file_iocs : FileIOCS<'a>,
+    cve_iocs : Vec<CVEIOC<'a>>,
 }
 
 pub fn parse_all_iocs(input: &str) -> IOCS {
@@ -28,5 +33,6 @@ pub fn parse_all_iocs(input: &str) -> IOCS {
         network_iocs : parse_network_iocs(input),
         hash_iocs : parse_hash_iocs(input),
         file_iocs : parse_file_iocs(input),
+        cve_iocs : parse_cve(input),
     }
 }
