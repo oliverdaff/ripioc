91692c14b0b77886601fae15c2671e26fe07020e
Oliver Daff <oliverdaff@users.noreply.github.com>
2020-06-04 14:36:56 +1000

    documentation of hash iocs

--- src/hash_ioc.rs
+++ src/hash_ioc.rs
@@ -1,3 +1,32 @@
+//! Contains patterns to match hash indicators found in the input text.
+//!
+//! This module can be used to extract single hash patterns from
+//! the text, using specific methods, ot to extract all hash patterns
+//! using the `parse_hash_iocs` method.
+//!
+//! # Examples
+//!
+//! ## Extract all hash IOCs from the input text
+//! ```
+//! use ripioc::hash_ioc::parse_hash_iocs;
+//!
+//! let iocs = parse_hash_iocs("check for a6b2fa823815336bb7352b02a93c970df51f66e8");
+//! ```
+//!
+//! ### Extract only the MD5 hash patterns
+//! ```
+//! use ripioc::hash_ioc::parse_md5;
+//! let md5_iocs = parse_md5(
+//!             "the sample contained\
+//!             08f2eb5f1bcbaf25ba97aef26593ed96 ");
+//! ```
+//!
+//! # Hash types extracted
+//! *   MD5
+//! *   SHA1
+//! *   SHA256
+//! *   SHA512
+//! *   SSDEEP
 use crate::regex_builder::compile_re;
 
 use std::boxed::Box;
@@ -6,15 +35,22 @@ use regex::Regex;
 use regex::RegexSet;
 use regex::RegexSetBuilder;
 
+/// The types of hashes searched for in the input text.
 #[derive(Debug, PartialEq, Eq)]
 pub enum HashIOC<'a> {
+    /// MD5 hash patterns.
     MD5(&'a str),
+    /// SHA1 hash patterns.
     SHA1(&'a str),
+    /// SHA256 hash patterns.
     SHA256(&'a str),
+    /// SHA512 hash patterns.
     SHA512(&'a str),
+    /// SSDEEP hash patterns.
     SSDEEP(&'a str),
 }
 
+///A set of hash patterns found in the input text.
 #[derive(Debug, PartialEq, Eq)]
 pub struct HashIOCS<'a> {
     md5s: Vec<HashIOC<'a>>,
@@ -24,16 +60,21 @@ pub struct HashIOCS<'a> {
     ssdeeps: Vec<HashIOC<'a>>,
 }
 
-pub const MD5_PATTERN: &'static str = r#"\b[A-Fa-f0-9]{32}\b"#;
+const MD5_PATTERN: &'static str = r#"\b[A-Fa-f0-9]{32}\b"#;
 
-pub const SHA1_PATTERN: &'static str = r#"\b[A-Fa-f0-9]{40}\b"#;
+const SHA1_PATTERN: &'static str = r#"\b[A-Fa-f0-9]{40}\b"#;
 
-pub const SHA256_PATTERN: &'static str = r#"\b[A-Fa-f0-9]{64}\b"#;
+const SHA256_PATTERN: &'static str = r#"\b[A-Fa-f0-9]{64}\b"#;
 
-pub const SHA512_PATTERN: &'static str = r#"\b[A-Fa-f0-9]{128}\b"#;
+const SHA512_PATTERN: &'static str = r#"\b[A-Fa-f0-9]{128}\b"#;
 
-pub const SSDEEP_PATTERN: &'static str = r#"\d{2}:[A-Za-z0-9/+]{3,}:[A-Za-z0-9/+]{3,}"#;
+const SSDEEP_PATTERN: &'static str = r#"\d{2}:[A-Za-z0-9/+]{3,}:[A-Za-z0-9/+]{3,}"#;
 
+///Parse all MD5 IOCs found in the input text.
+/// # Arguments
+/// * input - input text to parse
+/// # Return
+/// A vector of MD5 IOCs found in the input text.
 pub fn parse_md5(input: &str) -> Vec<HashIOC> {
     lazy_static! {
         static ref MD5_RE: Box<Regex> = compile_re(MD5_PATTERN);
@@ -44,6 +85,11 @@ pub fn parse_md5(input: &str) -> Vec<HashIOC> {
         .collect();
 }
 
+///Parse all SHA1 IOCs found in the input text.
+/// # Arguments
+/// * input - input text to parse
+/// # Return
+/// A vector of SHA1 IOCs found in the input text.
 pub fn parse_sha1(input: &str) -> Vec<HashIOC> {
     lazy_static! {
         static ref SHA1_RE: Box<Regex> = compile_re(SHA1_PATTERN);
@@ -53,7 +99,11 @@ pub fn parse_sha1(input: &str) -> Vec<HashIOC> {
         .map(|x| HashIOC::SHA1(x.as_str()))
         .collect();
 }
-
+///Parse all SHA256 IOCs found in the input text.
+/// # Arguments
+/// * input - input text to parse
+/// # Return
+/// A vector of SHA256 IOCs found in the input text.
 pub fn parse_sha256(input: &str) -> Vec<HashIOC> {
     lazy_static! {
         static ref SHA256_RE: Box<Regex> = compile_re(SHA256_PATTERN);
@@ -65,6 +115,11 @@ pub fn parse_sha256(input: &str) -> Vec<HashIOC> {
         .collect()
 }
 
+///Parse all SHA512 IOCs found in the input text.
+/// # Arguments
+/// * input - input text to parse
+/// # Return
+/// A vector of SHA512 IOCs found in the input text.
 pub fn parse_sha512(input: &str) -> Vec<HashIOC> {
     lazy_static! {
         static ref SHA512_RE: Box<Regex> = compile_re(SHA512_PATTERN);
@@ -76,6 +131,11 @@ pub fn parse_sha512(input: &str) -> Vec<HashIOC> {
         .collect()
 }
 
+///Parse all SSDEEP IOCs found in the input text.
+/// # Arguments
+/// * input - input text to parse
+/// # Return
+/// A vector of SSDEEP IOCs found in the input text.
 pub fn parse_ssdeep(input: &str) -> Vec<HashIOC> {
     lazy_static! {
         static ref SSDEEP_RE: Box<Regex> = compile_re(SSDEEP_PATTERN);
@@ -87,6 +147,12 @@ pub fn parse_ssdeep(input: &str) -> Vec<HashIOC> {
         .collect()
 }
 
+///Parse all hash IOCs found in the input text.
+/// # Arguments
+/// * input - input text to parse
+/// # Return
+/// A ['HashIOCS`](struct.HashIOCS.html) struct containing
+/// all the hash IOCs found in the input text.
 pub fn parse_hash_iocs(input: &str) -> HashIOCS {
     lazy_static! {
         static ref HASH_PATTERNS: RegexSet  = RegexSetBuilder::new(
@@ -125,7 +191,7 @@ pub fn parse_hash_iocs(input: &str) -> HashIOCS {
         } else {
             vec![]
         },
-        ssdeeps: if matches.matched(3) {
+        ssdeeps: if matches.matched(4) {
             parse_ssdeep(input)
         } else {
             vec![]
